a2bae05ba0c04a4f2fa352ceb67f0880fc002a87
Uriel Morales Jimenez <uriel.morales@stkbancomerans.com>
2020-03-18 17:48:38 -0600

    Se agregan carpetas y componentes para plantilla del proyecto

---
+++ src/components/Report.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Report extends Component
+{
+  render(){
+    return(
+      <div className="Report">
+        <h1>Soy la seccion del reporteReporte</h1>
+      </div>
+    );
+  }
+}
+export default Report;
